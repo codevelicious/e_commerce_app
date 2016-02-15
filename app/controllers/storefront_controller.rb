34bff3874f6e542b89d8f9f2ab747fafc2258dbc
Yovel K. Johns <codevelicious@gmail.com>
2016-02-15 08:41:07 -0800

    First Commit

---
+++ app/controllers/storefront_controller.rb
@@ -0,0 +1,17 @@
+class StorefrontController < ApplicationController
+ def all_items
+     @products = Product.all
+ end
+
+ def items_by_category
+     # link to this page would be:
+     # <%= link_to categorical_path(category_id: product.category.id) %>
+     @products = Product.where(category: params[:category_id])
+ end
+
+ def items_by_brand
+     # link to this page would be:
+     # <%= link_to categorical_path(brand: product.brand) %>
+     @products = Product.where(brand: params[:brand])
+ end
+end
